3f40edc1253480d771ae41e6a37d61cab3936e35
Denis Volpato Martins <denisvm@gmail.com>
2016-06-30 18:34:43 -0300

    travis tests

--- .travis.libucl.sh
+++ .travis.libucl.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 set -e
 
-LIBUCL_DEFAULT_REPO=git://github.com/vstakhov/libucl
-LIBUCL_DEFAULT_BRANCH=master
-
-LIBUCL_REPO=${LIBUCL_REPO:-$LIBUCL_DEFAULT_REPO}
-LIBUCL_BRANCH=${LIBUCL_BRANCH:-$LIBUCL_DEFAULT_BRANCH}
+: ${LIBUCL_REPO="git://github.com/vstakhov/libucl"}
+: ${LIBUCL_BRANCH="master"}
 
 git clone --depth=1 -b "$LIBUCL_BRANCH" "$LIBUCL_REPO" vendor/libucl
 cd vendor/libucl
